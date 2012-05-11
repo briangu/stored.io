4ad3b0ef20f6f7b60c841baac1c03660c0166f1d
Brian Guarraci <bguarraci@linkedin.com>
2012-05-11 16:38:26 -0700

    update

--- src/main/scala/io/stored/server/Node.scala
+++ src/main/scala/io/stored/server/Node.scala
@@ -7,6 +7,8 @@ import org.jboss.netty.handler.codec.http.HttpResponseStatus
 import org.json.JSONObject
 import java.util.Map
 import collection.mutable.HashSet
+import java.security.MessageDigest
+import com.sun.org.apache.xerces.internal.impl.dv.util.HexBin
 
 
 class Node(val ids: Set[Int], val schema: Schema) {
@@ -17,7 +19,7 @@ object Node {
 
   var node: Node = null
 
-  def flatten(jsonData: JSONObject) : Map[String, String] = {
+  def flatten(jsonData: JSONObject) : Map[String, AnyRef] = {
     null
   }
 
@@ -25,12 +27,12 @@ object Node {
     0
   }
 
-  def getTargetNodeIdFromData(data: Map[String, String], schema: Schema) : Int = {
-    val hashCoords = applyHSHashing(data, schema)
+  def getTargetNodeIdFromData(data: Map[String, AnyRef], schema: Schema) : Int = {
+    val hashCoords = hashSchemaFields(data, schema)
     0
   }
 
-  def applyHSHashing(data: Map[String, String], schema: Schema) : Map[String, Array[Byte]] = {
+  def hashSchemaFields(data: Map[String, AnyRef], schema: Schema) : Map[String, Array[Byte]] = {
     null
   }
 
@@ -38,19 +40,19 @@ object Node {
 
   }
 
-  def storeData(nodeId: Int, data: Map[String, String]) {
+  def indexData(nodeId: Int, data: Map[String, AnyRef], rawData: String) {
     if (node.ids.contains(nodeId)) {
-      storeData(data)
+      storeData(data, rawData)
     } else {
-      storeData(findNodeHost(nodeId), data)
+      storeData(findNodeHost(nodeId), data, rawData)
     }
   }
 
-  def storeData(host: String, data: Map[String, String]) {
+  def storeData(host: String, data: Map[String, AnyRef], rawData: String) {
 
   }
 
-  def storeData(data: Map[String, String]) {
+  def storeData(data: Map[String, AnyRef], rawData: String) {
     //ensureColumnsExist(data)
     // can be done implicitly as we fill out the write request
   }
@@ -61,9 +63,36 @@ object Node {
 
   def determineNodeIds(dimensions: Int) : Set[Int] = {
     val ids = new HashSet[Int]
-    // for 2^dimensions, store every node
-//    for (x <- 0 .. Math.)
+    for (x <- 0 until math.pow(2, dimensions)) ids.add(x)
     ids.toSet
+  }
+
+  def md5Hash(datum: Array[Byte]) : BigInt = {
+    val m = MessageDigest.getInstance("MD5");
+    m.reset();
+    m.update(datum);
+    val digest = m.digest();
+    BigInt.apply(digest)
+  }
+
+  def getNBits(bi: BigInt, dim: Int) : Int = {
+     (bi & (math.pow(2,dim)-1).toInt).intValue()
+  }
+
+  def getNBits(data: Array[Byte], dim: Int) : Int = {
+    getNBits(BigInt.apply(data), dim)
+  }
+
+  def getDataHash(data: String) : Array[Byte] = {
+    null
+  }
+
+  def storeData(nodeId: Int, data: String) {
+
+  }
+
+  def getData(nodeId: Int, hash: String) : String = {
+    null
   }
 
   def main(args: Array[String]) {
@@ -78,6 +107,10 @@ object Node {
           }
         })
 
+        get("/schema", new RouteHandler {
+          def exec(args: Map[String, String]) = new JsonResponse(new JSONObject())
+        })
+
         // transform map into flat namespace map
         // perform hyperspace hashing on named fields using schema reference
         // store datum into table
@@ -87,12 +120,38 @@ object Node {
         //  in the localhost case, we hold all shards on one Node
         //  in the multi-node case, we'd have to forward the request to teh appropriate node based on hashCoords
         //
+        post("/index", new RouteHandler {
+          def exec(args: Map[String, String]): RouteResponse = {
+            if (!args.containsKey("data")) return new StatusResponse(HttpResponseStatus.BAD_REQUEST)
+            val rawData = args.get("data")
+            val data = flatten(new JSONObject(rawData))
+            val nodeId = getTargetNodeIdFromData(data, node.schema)
+            indexData(nodeId, data, rawData)
+            new StatusResponse(HttpResponseStatus.OK)
+          }
+        })
+
+        get("/index/$query", new RouteHandler {
+          def exec(args: Map[String, String]) = new JsonResponse(new JSONObject())
+        })
+
         post("/data", new RouteHandler {
           def exec(args: Map[String, String]): RouteResponse = {
-            val data = flatten(new JSONObject(args.get("data")))
-            val nodeId = getTargetNodeIdFromData(data, node.schema)
-            storeData(nodeId, data)
+            if (!args.containsKey("data")) return new StatusResponse(HttpResponseStatus.BAD_REQUEST)
+            val rawData = args.get("data")
+            val hash = getDataHash(rawData)
+            val nodeId = getNBits(hash, node.schema.dimensions)
+            storeData(nodeId, rawData)
             new StatusResponse(HttpResponseStatus.OK)
+          }
+        })
+
+        get("/data/$hash", new RouteHandler {
+          def exec(args: Map[String, String]) = {
+            val hash = args.get("hash")
+            val nodeId = getNBits(HexBin.decode(hash), node.schema.dimensions)
+            val data = getData(nodeId, hash)
+            new JsonResponse(new JSONObject(data))
           }
         })
       }
