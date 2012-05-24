6ddcee2a5c4c0adf7ae23203b6087dc31336b9d6
Brian Guarraci <bguarraci@linkedin.com>
2012-05-24 15:46:27 -0700

    quiet

--- src/main/scala/io/stored/server/Node.scala
+++ src/main/scala/io/stored/server/Node.scala
@@ -82,7 +82,7 @@ object Node {
         } else if (dataVal.isInstanceOf[Double]) {
           result.put(key, List(ProjectionField.md5Hash(dataVal.asInstanceOf[Double])))
         } else {
-          println("not hashing col: " + key)
+          // println("not hashing col: " + key)
           // skip fields we can't hash
         }
       }
