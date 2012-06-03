51447088506b9c5ed8077a9f55b45381d990ebdd
Brian Guarraci <brian@ops5.com>
2012-06-02 21:06:41 -0700

    settle on port 8080 as default

--- bin/launch.sh
+++ bin/launch.sh
@@ -1,1 +1,1 @@
-java -Xmx512M -jar target/io.stored-0.0.1-SNAPSHOT-jar-with-dependencies.jar $1 db/$1 src/main/resources/nodes.json src/main/resources/projections.json
+java -Xmx512M -jar dist/io.stored-0.0.1-SNAPSHOT-jar-with-dependencies.jar $1 db/$1 src/main/resources/nodes.json src/main/resources/projections.json
--- src/main/resources/add.sh
+++ src/main/resources/add.sh
@@ -1,7 +1,7 @@
 echo "add red car"
-curl -d@record.json http://localhost:7080/records
+curl -d@record.json http://localhost:8080/records
 echo "add blue car"
-curl -d@record2.json http://localhost:7080/records
+curl -d@record2.json http://localhost:8080/records
 echo "add tweet"
-curl -d@tweet.json http://localhost:7080/records
+curl -d@tweet.json http://localhost:8080/records
 
