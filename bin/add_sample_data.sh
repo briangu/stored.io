eada09dd3fce4e8650330815628dc219f689a308
Brian Guarraci <brian@ops5.com>
2012-06-07 21:52:01 -0700

    update scripts

--- bin/add_sample_data.sh
+++ bin/add_sample_data.sh
@@ -1,7 +1,4 @@
-echo "add red car"
-curl -d@src/main/resources/record.json http://localhost:8080/records
-echo "add blue car"
-curl -d@src/main/resources/record2.json http://localhost:8080/records
+echo "adding cars"
+curl -d@src/main/resources/records.json http://localhost:8080/records
 echo "add tweet"
 curl -d@src/main/resources/tweet.json http://localhost:8080/records
-
